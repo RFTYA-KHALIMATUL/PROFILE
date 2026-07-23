25643719f4cd67bc0e5e6f45a6179b09a9233571
RFTYA-KHALIMATUL <retyakhalimatul@gmail.com>
2026-07-23 13:56:03 +0700

    Add files via upload

---
+++ component.js
@@ -0,0 +1,11 @@
+export class Component{
+    constructor(props ={}){
+        //props data yg di krim dri luar komponen
+    this.props = props;
+        //props yg dikls sma dngn props di cons
+    }
+    render() {
+        throw new Error(`render() class belum di buat`);
+        
+    }
+}
